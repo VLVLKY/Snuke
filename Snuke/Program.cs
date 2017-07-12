9c1960ec9df1c1db3321dceeb93e5e4ed71733e2
vv <kyvv@bk.ru>
2017-07-12 22:03:04 +0400

    Hello World

---
+++ Snuke/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snuke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
